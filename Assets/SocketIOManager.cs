9c0eb41d92cc2fc99c096da6c095b7c256e4f394
Xizi Lucy Wang <xiziwang@umich.edu>
2019-04-09 23:16:15 -0400

    wrote a text input canvas to enter ip address and player id

--- Assets/SocketIOManager.cs
+++ Assets/SocketIOManager.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SocketIOManager : MonoBehaviour {
+    public GameObject playerIDInputFieldText;
+    public GameObject serverIPAddressInputFieldText;
+    public GameObject statusText;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -14,4 +18,13 @@ public class SocketIOManager : MonoBehaviour {
 		
 	}
 
+    public void ConnectServer()
+    {
+        string playerIDInput = playerIDInputFieldText.GetComponent<Text>().text;
+        string serverIPAddress = "ws://" + serverIPAddressInputFieldText.GetComponent<Text>().text + ":8080/socket.io/?EIO=4&transport=websocket";
+        Debug.Log(serverIPAddress);
+        // TODO: connect to the server
+
+        // TODO: change status text
+}
 }
