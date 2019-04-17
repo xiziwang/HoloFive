4b4d60b5aa7bcfa04bfbd3f55b429ad8708e0ba4
Xizi Lucy Wang <xiziwang@umich.edu>
2019-04-17 02:09:01 -0400

    working version; need to add sound & disenable model before the other player joined in

--- Assets/ActionManager.cs
+++ Assets/ActionManager.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ActionManager : MonoBehaviour {
+    private SocketIOManager socketIOManager;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        socketIOManager = GameObject.Find("SocketIOCanvas").GetComponent<SocketIOManager>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -20,7 +22,7 @@ public class ActionManager : MonoBehaviour {
         {
 
             // TODO: Send msg to phone
-            Debug.Log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+            socketIOManager.SendMsg();
         }
     }
 }
--- Assets/SocketIOManager.cs
+++ Assets/SocketIOManager.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using BestHTTP;
+using BestHTTP.SocketIO;
 
 public class SocketIOManager : MonoBehaviour {
     public GameObject playerIDInputFieldText;
     public GameObject serverIPAddressInputFieldText;
     public GameObject statusText;
-
+    private SocketManager manager;
     // Use this for initialization
     void Start () {
 		
@@ -20,11 +23,24 @@ public class SocketIOManager : MonoBehaviour {
 
     public void ConnectServer()
     {
-        string playerIDInput = playerIDInputFieldText.GetComponent<Text>().text;
-        string serverIPAddress = "ws://" + serverIPAddressInputFieldText.GetComponent<Text>().text + ":8080/socket.io/?EIO=4&transport=websocket";
+        string serverIPAddress = "http://" + serverIPAddressInputFieldText.GetComponent<Text>().text + ":8080/socket.io/";
         Debug.Log(serverIPAddress);
         // TODO: connect to the server
+        try{
+            manager = new SocketManager(new Uri(serverIPAddress));
+            statusText.GetComponent<Text>().text = "Status: Connected";
+        }
+        catch
+        {
+            Debug.Log("An Error");
+        }
 
         // TODO: change status text
-}
+    }
+    
+    public void SendMsg()
+    {
+        string playerIDInput = playerIDInputFieldText.GetComponent<Text>().text;
+        manager.Socket.Emit("message", playerIDInput, "");
+    }
 }
